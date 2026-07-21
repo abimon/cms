8fd9bdcc4eaa7dbb9f39aa27823bd74356096054
abimon <eddyabby59@gmail.com>
2026-07-22 00:25:38 +0300

    unauthenticate payment callback route

--- database/migrations/2026_07_18_174417_create_mpesas_table.php
+++ database/migrations/2026_07_18_174417_create_mpesas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('mpesas', function (Blueprint $table) {
             $table->id();
             $table->string('TransactionType');
-            $table->foreignId('payment_id');
+            $table->string('payment_id');
             $table->string('TransAmount');
             $table->string('MpesaReceiptNumber');
             $table->string('TransactionDate');
